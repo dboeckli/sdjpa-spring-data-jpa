1be8109551b9fa98d6611590b9f88518e8c5d044
adm-boeckli <dboeckli@gmail.com>
2025-04-02 08:50:50 +0200

    implement crud methods

--- src/main/java/ch/dboeckli/guru/jpa/hibernate/dao/dao/AuthorDaoImpl.java
+++ src/main/java/ch/dboeckli/guru/jpa/hibernate/dao/dao/AuthorDaoImpl.java
@@ -1,42 +1,44 @@
 package ch.dboeckli.guru.jpa.hibernate.dao.dao;
 
 import ch.dboeckli.guru.jpa.hibernate.dao.domain.Author;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.Query;
-import jakarta.persistence.TypedQuery;
-import jakarta.persistence.criteria.*;
+import ch.dboeckli.guru.jpa.hibernate.dao.repository.AuthorRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-
-import java.util.List;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @RequiredArgsConstructor
 public class AuthorDaoImpl implements AuthorDao {
 
+    private final AuthorRepository authorRepository;
+
     @Override
     public Author getById(Long id) {
-        return null;
+        return authorRepository.getReferenceById(id);
     }
 
     @Override
     public Author findAuthorByName(String firstName, String lastName) {
+        // TODO: NOT IMPLEMENTED YET
         return null;
     }
 
     @Override
     public Author saveNewAuthor(Author author) {
-        return null;
+        return authorRepository.save(author);
     }
 
     @Override
+    @Transactional
     public Author updateAuthor(Author author) {
-        return null;
+        Author foundAuthor = authorRepository.getReferenceById(author.getId());
+        foundAuthor.setFirstName(author.getFirstName());
+        foundAuthor.setLastName(author.getLastName());
+        return authorRepository.save(foundAuthor);
     }
 
     @Override
     public void deleteAuthorById(Long id) {
-
+        authorRepository.deleteById(id);
     }
 }
---
+++ src/test/java/ch/dboeckli/guru/jpa/hibernate/dao/dao/h2/AuthorDaoImplTest.java
@@ -0,0 +1,84 @@
+package ch.dboeckli.guru.jpa.hibernate.dao.dao.h2;
+
+import ch.dboeckli.guru.jpa.hibernate.dao.dao.AuthorDao;
+import ch.dboeckli.guru.jpa.hibernate.dao.dao.AuthorDaoImpl;
+import ch.dboeckli.guru.jpa.hibernate.dao.domain.Author;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.orm.jpa.JpaObjectRetrievalFailureException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@Import({ AuthorDaoImpl.class })
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@DataJpaTest
+@Slf4j
+class AuthorDaoImplTest {
+
+    @Autowired
+    AuthorDao authorDao;
+
+    @Test
+    void testDeleteAuthor() {
+        Author author = new Author();
+        author.setFirstName("john");
+        author.setLastName("t");
+
+        Author saved = authorDao.saveNewAuthor(author);
+
+        authorDao.deleteAuthorById(saved.getId());
+
+        assertThrows(JpaObjectRetrievalFailureException.class, () -> {
+            authorDao.getById(saved.getId());
+        });
+
+    }
+
+    @Test
+    void testUpdateAuthor() {
+        Author author = new Author();
+        author.setFirstName("john");
+        author.setLastName("t");
+
+        Author saved = authorDao.saveNewAuthor(author);
+
+        saved.setLastName("Thompson");
+        Author updated = authorDao.updateAuthor(saved);
+
+        assertThat(updated.getLastName()).isEqualTo("Thompson");
+    }
+
+    @Test
+    void testSaveAuthor() {
+        Author author = new Author();
+        author.setFirstName("John");
+        author.setLastName("Thompson");
+        Author saved = authorDao.saveNewAuthor(author);
+
+        assertThat(saved).isNotNull();
+    }
+
+    @Test
+    @Disabled("not implemented yet")
+    void testGetAuthorByName() {
+        // TODO: NOT IMPLEMENTED YET
+        Author author = authorDao.findAuthorByName("Craig", "Walls");
+
+        assertThat(author).isNotNull();
+    }
+
+    @Test
+    void testGetAuthor() {
+
+        Author author = authorDao.getById(1L);
+
+        assertThat(author).isNotNull();
+
+    }
+}
