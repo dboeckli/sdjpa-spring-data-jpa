d7bdcd3b3c452b8ad9dd1d6e83271de349e9179a
adm-boeckli <dboeckli@gmail.com>
2025-04-03 11:23:30 +0200

    impl of paging and sorting for book

--- src/main/java/ch/dboeckli/guru/jpa/hibernate/dao/dao/BookDaoImpl.java
+++ src/main/java/ch/dboeckli/guru/jpa/hibernate/dao/dao/BookDaoImpl.java
@@ -4,6 +4,8 @@ import ch.dboeckli.guru.jpa.hibernate.dao.domain.Book;
 import ch.dboeckli.guru.jpa.hibernate.dao.repository.BookRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -29,25 +31,32 @@ public class BookDaoImpl implements BookDao {
 
     @Override
     public List<Book> findAllBooks() {
-        // TODO: Implement pagination and tests
-        return List.of();
+        return bookRepository.findAll();
     }
+
 
     @Override
     public List<Book> findAllBooks(int pageSize, int offset) {
-        // TODO: Implement pagination and tests
-        return List.of();
+        Pageable pageable = PageRequest.ofSize(pageSize);
+
+        if (offset > 0) {
+            pageable = pageable.withPage(offset / pageSize);
+        } else {
+            pageable = pageable.withPage(0);
+        }
+
+        return this.findAllBooks(pageable);
     }
 
     @Override
     public List<Book> findAllBooks(Pageable pageable) {
-        // TODO: Implement pagination and tests
-        return List.of();
+        return bookRepository.findAll(pageable).getContent();
     }
 
     @Override
     public List<Book> findAllBooksSortByTitle(Pageable pageable) {
-        return List.of();
+        Page<Book> bookPage = bookRepository.findAll(pageable);
+        return bookPage.getContent();
     }
 
     @Override
